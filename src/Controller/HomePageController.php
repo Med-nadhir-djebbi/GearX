f872c98a17e0d6ca199834c67bcdc8d3636d6308
aziz-haddadi <mohamedaziz.haddadi@insat.ucar.tn>
2025-05-13 22:36:04 +0100

    firstcommitt

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,4 +17,64 @@ final class HomePageController extends AbstractController
             'controller_name' => 'HomePageController',
         ]);
     }
+    #[Route('/template', name: 'tempp')]
+    public function about(EntityManagerInterface $entityManager): Response
+    {
+        // Sample categories data
+        $categories = [
+            'Informatique' => [
+                'Ordinateurs portables',
+                'PC de bureau',
+                'Composants PC',
+                'Périphériques'
+            ],
+            'Smartphones' => [
+                'Apple',
+                'Samsung',
+                'Xiaomi',
+                'OnePlus'
+            ],
+            'Gaming' => [
+                'Consoles',
+                'Jeux vidéo',
+                'Accessoires gaming',
+                'PC Gaming'
+            ],
+            'Audio' => [
+                'Casques',
+                'Écouteurs',
+                'Enceintes',
+                'Microphones'
+            ]
+        ];
+
+        // Check if categories already exist
+        $existingCategories = $entityManager->getRepository(Category::class)->findAll();
+
+        if (empty($existingCategories)) {
+            // Create categories and subcategories
+            foreach ($categories as $categoryName => $subcategories) {
+                $category = new Category();
+                $category->setName($categoryName);
+                $entityManager->persist($category);
+
+                foreach ($subcategories as $subcategoryName) {
+                    $subcategory = new Category();
+                    $subcategory->setName($subcategoryName);
+                    $subcategory->setParent($category);
+                    $entityManager->persist($subcategory);
+                }
+            }
+
+            $entityManager->flush();
+        }
+
+        // Get all main categories with their subcategories
+        $mainCategories = $entityManager->getRepository(Category::class)->findBy(['parent' => null]);
+
+        return $this->render('base.html.twig', [
+            'categories' => $mainCategories
+        ]);
+    }
+
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,11 +1,13 @@
 <?php
+// src/Entity/Category.php
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ORM\Entity]
 class Category
 {
     #[ORM\Id]
@@ -13,9 +15,21 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'subcategories')]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $subcategories;
+
+    public function __construct()
+    {
+        $this->subcategories = new ArrayCollection();
+    }
+
+    // Getters and setters
     public function getId(): ?int
     {
         return $this->id;
@@ -26,10 +40,34 @@ class Category
         return $this->name;
     }
 
-    public function setName(string $name): static
+    public function setName(string $name): self
     {
         $this->name = $name;
+        return $this;
+    }
 
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    public function getSubcategories(): Collection
+    {
+        return $this->subcategories;
+    }
+
+    public function addSubcategory(self $category): self
+    {
+        if (!$this->subcategories->contains($category)) {
+            $this->subcategories[] = $category;
+            $category->setParent($this);
+        }
         return $this;
     }
 }
