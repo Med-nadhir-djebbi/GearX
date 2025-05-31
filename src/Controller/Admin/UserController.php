e486eb4917bfdab667235da3bbad09ec501dcddc
aziz-haddadi <mohamedaziz.haddadi@insat.ucar.tn>
2025-05-31 01:16:06 +0100

    all_crud_are_done

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -3,119 +3,120 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-#[Route('/admin/users', name: 'admin_users_')]
-#[IsGranted('ROLE_ADMIN')]
+#[Route('/admin/users')]
 class UserController extends AbstractController
 {
-    private $entityManager;
-    private $userRepository;
-    private $passwordHasher;
-
     public function __construct(
-        EntityManagerInterface $entityManager, 
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher
-    ) {
-        $this->entityManager = $entityManager;
-        $this->userRepository = $userRepository;
-        $this->passwordHasher = $passwordHasher;
-    }
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
 
-    #[Route('', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/', name: 'admin_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('admin/users.html.twig', [
-            'users' => $this->userRepository->findAll(),
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'admin_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
-        if ($request->isMethod('POST')) {
-            $user = new User();
-            $user->setUsername($request->request->get('username'));
-            $user->setEmail($request->request->get('email'));
-            $user->setPassword(
-                $this->passwordHasher->hashPassword(
-                    $user,
-                    $request->request->get('password')
-                )
-            );
-            
-            $roles = (array)$request->request->get('roles', []);
-            if (in_array('ROLE_ADMIN', $roles)) {
-                $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
-            } else {
-                $user->setRoles(['ROLE_USER']);
-            }
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hash the password
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+            $user->setPassword($hashedPassword);
+            
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
             $this->addFlash('success', 'User created successfully.');
-            return $this->redirectToRoute('admin_users_index');
+            return $this->redirectToRoute('admin_user_index');
         }
 
-        return $this->render('admin/user/new.html.twig');
+        return $this->render('admin/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 
-    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}', name: 'admin_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user): Response
     {
-        if ($request->isMethod('POST')) {
-            $user->setUsername($request->request->get('username'));
-            $user->setEmail($request->request->get('email'));
-            
-            if ($password = $request->request->get('password')) {
-                $user->setPassword(
-                    $this->passwordHasher->hashPassword(
-                        $user,
-                        $password
-                    )
+        // Prevent editing of super admin by other admins
+        if ($user->getRoles() !== $this->getUser()->getRoles() && in_array('ROLE_SUPER_ADMIN', $user->getRoles())) {
+            throw new AccessDeniedException('You cannot edit a super admin account.');
+        }
+
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Only hash password if it was changed
+            if ($plainPassword = $form->get('plainPassword')->getData()) {
+                $hashedPassword = $this->passwordHasher->hashPassword(
+                    $user,
+                    $plainPassword
                 );
-            }
-            
-            $roles = (array)$request->request->get('roles', []);
-            if (in_array('ROLE_ADMIN', $roles)) {
-                $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
-            } else {
-                $user->setRoles(['ROLE_USER']);
+                $user->setPassword($hashedPassword);
             }
 
             $this->entityManager->flush();
 
             $this->addFlash('success', 'User updated successfully.');
-            return $this->redirectToRoute('admin_users_index');
+            return $this->redirectToRoute('admin_user_index');
         }
 
         return $this->render('admin/user/edit.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
-    public function delete(User $user): Response
+    #[Route('/{id}', name: 'admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user): Response
     {
-        // Don't allow deleting admin users
-        if (in_array('ROLE_ADMIN', $user->getRoles())) {
-            $this->addFlash('error', 'Cannot delete admin users.');
-            return $this->redirectToRoute('admin_users_index');
+        // Prevent deletion of super admin by other admins
+        if (in_array('ROLE_SUPER_ADMIN', $user->getRoles()) && !in_array('ROLE_SUPER_ADMIN', $this->getUser()->getRoles())) {
+            throw new AccessDeniedException('You cannot delete a super admin account.');
         }
 
-        $this->entityManager->remove($user);
-        $this->entityManager->flush();
+        // Prevent self-deletion
+        if ($user->getId() === $this->getUser()->getId()) {
+            throw new AccessDeniedException('You cannot delete your own account.');
+        }
 
-        $this->addFlash('success', 'User deleted successfully.');
-        return $this->redirectToRoute('admin_users_index');
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $this->entityManager->remove($user);
+            $this->entityManager->flush();
+            $this->addFlash('success', 'User deleted successfully.');
+        }
+
+        return $this->redirectToRoute('admin_user_index');
     }
 } 
---
+++ src/Form/UserType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email',
+                    ]),
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a username',
+                    ]),
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('lastName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Super Admin' => 'ROLE_SUPER_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+
+        if (!$options['is_edit']) {
+            $builder->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+        } else {
+            $builder->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
+                'help' => 'Leave empty to keep current password',
+            ]);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'is_edit' => false,
+        ]);
+    }
+} 
